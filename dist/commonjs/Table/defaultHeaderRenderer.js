715c927fb1942d5ef0976591a3841c5533e5fdb3
EijiHsu <apolkingg8@gmail.com>
2018-01-31 18:36:40 +0800

    update dist

---
+++ dist/commonjs/Table/defaultHeaderRenderer.js
@@ -0,0 +1,43 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = defaultHeaderRenderer;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _SortIndicator = require('./SortIndicator');
+
+var _SortIndicator2 = _interopRequireDefault(_SortIndicator);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Default table header renderer.
+ */
+function defaultHeaderRenderer(_ref) {
+  var dataKey = _ref.dataKey,
+      label = _ref.label,
+      sortBy = _ref.sortBy,
+      sortDirection = _ref.sortDirection;
+
+  var showSortIndicator = sortBy === dataKey;
+  var children = [_react2.default.createElement(
+    'span',
+    {
+      className: 'ReactVirtualized__Table__headerTruncatedText',
+      key: 'label',
+      title: label },
+    label
+  )];
+
+  if (showSortIndicator) {
+    children.push(_react2.default.createElement(_SortIndicator2.default, { key: 'SortIndicator', sortDirection: sortDirection }));
+  }
+
+  return children;
+}
+defaultHeaderRenderer.propTypes = process.env.NODE_ENV === 'production' ? null : require('./types').bpfrpt_proptype_HeaderRendererParams === require('prop-types').any ? {} : require('./types').bpfrpt_proptype_HeaderRendererParams;
